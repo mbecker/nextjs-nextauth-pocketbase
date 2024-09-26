93bb784f31225a1a83000157e46da14a28f5cfe2
Mats Becker <mats.becker@gmail.com>
2024-09-26 14:54:15 +0200

    add comment to console

--- components/auth/AuthLink.tsx
+++ components/auth/AuthLink.tsx
@@ -11,6 +11,7 @@ export default function AuthLink({
   authProvider: AuthProviderInfo;
   redirectURI: string;
 }) {
+  console.log(`AuthLink for ${authProvider.name} - redirect to: ${authProvider.authUrl + redirectURI}`)
   return (
     <button
       className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
