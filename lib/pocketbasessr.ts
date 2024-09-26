612fe32cf8fa014f29bd329d9c669d3f53338215
Mats Becker <mats.becker@gmail.com>
2024-09-26 14:54:31 +0200

    pocketbase autoCancellation to false

--- lib/pocketbasessr.ts
+++ lib/pocketbasessr.ts
@@ -1,7 +1,8 @@
 import PocketBase from "pocketbase";
 
 const pocketbase = new PocketBase(process.env.POCKETBASE_URL);
-if (process.env.NODE_ENV === "development") pocketbase.autoCancellation(false);
+// if (process.env.NODE_ENV === "development") pocketbase.autoCancellation(false);
+pocketbase.autoCancellation(false);
 
 const redirectURI = `${process.env.NEXTAUTH_URL}/api/auth/callback/pocketbase`;
 export { pocketbase, redirectURI };
