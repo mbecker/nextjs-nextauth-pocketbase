bed5bb332350240dc7af26250cdf1385c4beeed3
Mats Becker <mats.becker@gmail.com>
2024-09-27 10:25:31 +0200

    add footer

--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -1,16 +1,20 @@
 import Sidebar from "@/components/Sidebar";
-
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col bg-gray-50 dark:bg-black lg:flex-row">
       <Sidebar />
-      <div className="h-mobile-dvh lg:h-dvh flex-1">
-        <main className="h-mobile-dvh lg:h-dvh flex-1 ">
-          <div className="h-full overflow-y-auto bg-white ring-1 ring-gray-200 dark:border-none dark:bg-gray-900 dark:ring-gray-800 p-4 lg:p-6">
-            {children}
-          </div>
+      <div className="h-mobile-dvh lg:h-dvh flex-1 flex flex-col space-y-2 bg-white ring-1 ring-gray-200 dark:border-none dark:bg-gray-900 dark:ring-gray-800 p-4 lg:p-6">
+        <main className="flex-1">
+          {children}
         </main>
+        <footer className="w-full flex items-center justify-end py-2">
+          <Link target="_blank" href="https://github.com/mbecker/nextjs-nextauth-pocketbase">
+            <GitHubLogoIcon className="w-8 h-8"/>
+          </Link>
+        </footer>
       </div>
     </div>
   );
